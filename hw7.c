902f487ea20c09746481efc1ec9042e470b68cfa
kew120 <127210184+kew120@users.noreply.github.com>
2023-11-10 12:41:11 +0900

    Add files via upload

---
+++ hw7.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main()
+{
+	int arr1[6] = { 1, 2, 3 ,4 ,5 ,6 };
+	int arr2[6] = { 7, 8, 9, 10, 11, 12 };
+	int* ptr1 = arr1;
+	int* ptr2 = arr2;
+
+	printf("arr1: ");
+	for (int i = 0; i < 6; i++)
+		printf("%d ", arr1[i]);
+	printf("\n");
+	printf("arr2: ");
+	for (int i = 0; i < 6; i++)
+		printf("%d ", arr2[i]);
+
+	for (int i = 0; i < 6; i++)
+	{
+		int temp = ptr1[i];
+		ptr1[i] = ptr2[i];
+		ptr2[i] = temp;
+	}
+
+	printf("\n\nafter swap\n");
+	printf("arr1: ");
+	for (int i = 0; i < 6; i++)
+		printf("%d ", arr1[i]);
+	printf("\n");
+	printf("arr2: ");
+	for (int i = 0; i < 6; i++)
+		printf("%d ", arr2[i]);
+	return 0;
+}
