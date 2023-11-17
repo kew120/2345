3f793f718e2b525da0921ffffd51d18417120c0c
kew120 <127210184+kew120@users.noreply.github.com>
2023-11-17 13:45:35 +0900

    hw8.c

--- hw8.c
+++ hw8.c
@@ -1,19 +1,26 @@
 #include <stdio.h>
 #include <math.h>
 
+double standard_deviation(double* arr)
+{
+	int b = 0, c = 0;
+	for (int i = 0; i < 5; i++)
+		b += arr[i];
+	b /= 5;
+	for (int i = 0; i < 5; i++)
+		c += pow(arr[i] - b, 2);
+	c /= 5;
+	return sqrt(c);
+}
+
 int main()
 {
-	double a[5], b = 0, c = 0;
+	double a[5];
 	printf("Enter 5 real numbers: ");
 	for (int i = 0; i < 5; i++)
 	{
 		scanf("%lf", &a[i]);
-		b += a[i];
 	}
-	b /= 5;
-	for (int i = 0; i < 5; i++)
-		c += pow(a[i] - b, 2);
-	c /= 5;
-	printf("Standard Deviation = %.3lf", sqrt(c));
+	printf("Standard Deviation = %.3lf", standard_deviation(a));
 	return 0;
 }
